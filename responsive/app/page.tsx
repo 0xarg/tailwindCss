7d8de170df719c783fa745c5f938b49341e1c47d
anurag8484 <anuragpoonia.ap@gmail.com>
2025-12-05 11:15:29 +0530

    responsive done

--- responsive/app/page.tsx
+++ responsive/app/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Responsive from "./components/responsive";
+import Navbar from "./components/navbar";
 
 const Home = () => {
   return (
-    <div className="flex mx-auto justify-center  items-center h-screen">
-      <Responsive />
+    <div className=" ">
+      <Navbar />
     </div>
   );
 };
