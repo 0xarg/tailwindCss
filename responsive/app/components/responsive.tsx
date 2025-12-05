5bfbba743966e16e043af76fb954b600f74de0bf
anurag8484 <anuragpoonia.ap@gmail.com>
2025-12-05 10:34:41 +0530

    responsive added

---
+++ responsive/app/components/responsive.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Responsive = () => {
+  return (
+    <div className="flex flex-col md:flex-row  items-center gap-10">
+      <div className="size-40 rounded-2xl bg-neutral-200" />
+      <div className="size-40 rounded-2xl bg-red-200" />
+      <div className="size-40 rounded-2xl bg-blue-200" />
+    </div>
+  );
+};
+
+export default Responsive;
---
+++ responsive/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Responsive from "./components/responsive";
+
+const Home = () => {
+  return (
+    <div className="flex mx-auto justify-center  items-center h-screen">
+      <Responsive />
+    </div>
+  );
+};
+
+export default Home;
